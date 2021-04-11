4e1f39adcd2d455f5f4d9961c3c57b34453b91f1
natsumadan <madan.mohit209@gmail.com>
2021-04-11 17:01:04 +1000

    update

---
+++ src/components/App/App.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import Nav from '../Nav/Nav';
+import './App.css';
+
+ class App extends React.Component {
+     render() {
+         return (
+             <div className="app">
+                 {/* Nav Section*/}
+                 <Nav/>
+
+                 {/* Landing Page*/}
+
+                 {/* Challenge Section*/}
+
+                 {/* Footer*/} 
+             </div>
+         )
+
+     }
+ }
+
+
+
+ export default App;
